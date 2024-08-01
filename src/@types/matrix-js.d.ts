cbe5eb7c4703d0b62f87f654b7f6d3ece1cf9a6f
Pedr <pedr.browne@gmail.com>
2024-08-01 09:09:57 +0100

    fix: Matrix-js typings

--- src/@types/matrix-js.d.ts
+++ src/@types/matrix-js.d.ts
@@ -1,17 +1,27 @@
+class Callable extends Function {
+  private _bound
+
+  constructor() {
+    super('...args', 'return this._bound.call(...args)')
+    this._bound = this.bind(this)
+    return this._bound
+  }
+}
+
 declare module 'matrix-js' {
-  export type Matrix = {
-    (a: number, b: number): number
-    constructor(a: number, b: number)
-    inv: () => number[][]
+  export class Matrix extends Callable {
+    constructor(a: number, b: number): Matrix
     trans: () => number[][]
     gen: (i: number) => Matrix
     set: (i: number, i: number) => { to: (value: number) => number[][] }
     prod: (matrix: Matrix) => number[][]
+    inv: () => number[][]
+    call(i: number, j: number): number
   }
 
   export default function matrix(matrix: number[][]): Matrix
 
   type Size = (i: number, j: number) => number[][]
 
-  matrix.gen = (i: number) => Size
+  matrix.gen = (i: number) => Size(i)
 }
