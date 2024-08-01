f3a9e19821f1a58eabbcdb877dfec19cab3b3cb4
Pedr <pedr.browne@gmail.com>
2024-08-01 23:55:21 +0100

    chore: Enable coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: [`**/*.test.js`],
+    include: [`**/*.test.{js,ts}`],
     setupFiles: [`./tests/setup.js`],
+    coverage: {
+      provider: `v8`, // or 'v8'
+      reporter: [`text`],
+      reportOnFailure: true,
+      include: [`src/**`],
+    },
   },
 })
