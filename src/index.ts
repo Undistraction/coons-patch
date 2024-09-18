65909f294449fc08118de67e65f30e255f3c80d6
Pedr <pedr.browne@gmail.com>
2024-09-18 15:28:55 +0100

    fix: Add missing Points type export

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ export type {
   InterpolatePointOnCurve,
   InterpolatePointOnCurveFactory,
   Point,
+  Points,
 } from './types'
 
 // -----------------------------------------------------------------------------
