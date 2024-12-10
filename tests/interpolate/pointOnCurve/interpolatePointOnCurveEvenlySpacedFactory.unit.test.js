c819175832ef365e9da89e66684a0446c0904710
Pedr <pedr.browne@gmail.com>
2024-12-10 12:34:17 +0000

    chore: Fix spelling

--- tests/interpolate/pointOnCurve/interpolatePointOnCurveEvenlySpacedFactory.unit.test.js
+++ tests/interpolate/pointOnCurve/interpolatePointOnCurveEvenlySpacedFactory.unit.test.js
@@ -13,31 +13,29 @@ describe(`interpolatePointOnCurveEvenlySpacedFactory`, () => {
   }
 
   it(`should return a function`, () => {
-    const interplolatePointOnCurve =
-      interpolatePointOnCurveEvenlySpacedFactory()
-    expect(interplolatePointOnCurve).toBeInstanceOf(Function)
+    const interpolatePointOnCurve = interpolatePointOnCurveEvenlySpacedFactory()
+    expect(interpolatePointOnCurve).toBeInstanceOf(Function)
   })
 
-  describe(`with default precsion`, () => {
-    const interplolatePointOnCurve =
-      interpolatePointOnCurveEvenlySpacedFactory()
+  describe(`with default precision`, () => {
+    const interpolatePointOnCurve = interpolatePointOnCurveEvenlySpacedFactory()
     it(`returns the correct point on the surface`, () => {
-      expect(interplolatePointOnCurve(0, curve)).toEqual({
+      expect(interpolatePointOnCurve(0, curve)).toEqual({
         x: 0,
         y: 0,
       })
 
-      expect(interplolatePointOnCurve(1, curve)).toEqual({
+      expect(interpolatePointOnCurve(1, curve)).toEqual({
         x: 100,
         y: 0,
       })
 
-      expect(interplolatePointOnCurve(0.25, curve)).toEqual({
+      expect(interpolatePointOnCurve(0.25, curve)).toEqual({
         x: 24.4618783125335,
         y: -6.334387794544809,
       })
 
-      expect(interplolatePointOnCurve(0.75, curve)).toEqual({
+      expect(interpolatePointOnCurve(0.75, curve)).toEqual({
         x: 75.53812168746649,
         y: -6.33438779454481,
       })
@@ -45,12 +43,12 @@ describe(`interpolatePointOnCurveEvenlySpacedFactory`, () => {
   })
 
   describe(`with custom precision`, () => {
-    const interplolatePointOnCurve = interpolatePointOnCurveEvenlySpacedFactory(
-      { precision: 10 }
-    )
+    const interpolatePointOnCurve = interpolatePointOnCurveEvenlySpacedFactory({
+      precision: 10,
+    })
 
     it(`returns the correct point on the surface`, () => {
-      expect(interplolatePointOnCurve(0.25, curve)).toEqual({
+      expect(interpolatePointOnCurve(0.25, curve)).toEqual({
         x: 24.471119926759947,
         y: -6.335308505609451,
       })
